0cb7a9a1eb64fc5e2cdc2bf4318960ffe7759c66
Halkath <beliver297@gmail.com>
2025-11-11 09:13:40 +0530

    save

--- studentGrade.py
+++ studentGrade.py
@@ -1,19 +1,27 @@
-import sys;
-if len(sys.argv) == 5:
-   script_name = sys.argv[0]
-   name = sys.argv[1]
-   marks1 = sys.argv[2]
-   marks2 = sys.argv[3]
-   marks3= sys.argv[4]
-else:
-   script_name = sys.argv[0]
-   name = "nikhil"
-   marks1 = 20
-   marks2 = 30
-   marks3= 40
+import sys
 
-sum = marks1+marks2+marks3
-avg = int(sum/3)
-print(f"Sum {sum}\n")
-print(f"Average: {avg}")
-print(f"Thansk For Using program")
+if len(sys.argv) == 5:
+    name = sys.argv[0]
+    rollno = sys.argv[1]
+    mark1 = sys.argv[2]
+    mark2 = sys.argv[3]
+    mark3= sys.argv[4]
+    
+    print("User provided input values:")
+else:
+    name = "Nikhil"
+    rollno = "42"
+    mark1 = "35"
+    mark2 = "35"
+    mark3= "35"
+    print("No input given, using default values:")
+
+total = int(mark1) + int(mark2) + int(mark3) + int(mark4) + int(mark5)
+average = total / 5 
+print(" Name:", name)
+print("Student Name:", name)
+print("Subject 1 marks :", mark1)
+print("Subject 2 marks :", mark2)
+print("Subject 3 marks :", mark3)
+print("Total Marks:", total)
+print("Average Marks:", average)
